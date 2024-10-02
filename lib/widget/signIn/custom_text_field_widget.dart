d4b66a780106be9b4d4a190841f340e2d0940310
m7mdragab <mr341349@gmail.com>
2024-10-02 09:21:01 +0300

    edit CustomTextFieldWidget

--- lib/widget/signIn/custom_text_field_widget.dart
+++ lib/widget/signIn/custom_text_field_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFieldWidget extends StatelessWidget {
-  const CustomTextFieldWidget({super.key, required this.label});
+  const CustomTextFieldWidget({super.key, required this.label, this.onChange});
   final String label;
+  final void Function(String)? onChange;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      cursorColor: Colors.white,
+      cursorErrorColor: Colors.white,
+      onChanged: onChange,
       decoration: InputDecoration(
         labelText: label,
         labelStyle: const TextStyle(
