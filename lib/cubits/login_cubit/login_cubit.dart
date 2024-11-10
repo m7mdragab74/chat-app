e8a24ba2aabb1e1c999e2e119504ade60966e705
m7mdragab <mr341349@gmail.com>
2024-11-10 21:41:33 +0200

    login function

--- lib/cubits/login_cubit/login_cubit.dart
+++ lib/cubits/login_cubit/login_cubit.dart
@@ -1,6 +1,19 @@
 import 'package:chat_app/cubits/login_cubit/login_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitialState());
+
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    emit(LoginLoadingState());
+    try {
+      UserCredential user = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      emit(LoginSuccessState());
+    } on Exception catch (e) {
+      emit(LoginFailureState('there are error : $e'));
+    }
+  }
 }
--- lib/cubits/login_cubit/login_state.dart
+++ lib/cubits/login_cubit/login_state.dart
@@ -2,6 +2,12 @@ abstract class LoginState {}
 
 class LoginInitialState extends LoginState {}
 
+class LoginLoadingState extends LoginState {}
+
 class LoginSuccessState extends LoginState {}
 
-class LoginFailureState extends LoginState {}
+class LoginFailureState extends LoginState {
+  final String errorMessage;
+
+  LoginFailureState(this.errorMessage);
+}
