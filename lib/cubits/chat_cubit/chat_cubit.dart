87c748a7b80d1836add78140a020403b29b71fdf
m7mdragab <mr341349@gmail.com>
2024-11-13 08:43:58 +0200

    chat logic

--- lib/cubits/chat_cubit/chat_cubit.dart
+++ lib/cubits/chat_cubit/chat_cubit.dart
@@ -1,6 +1,24 @@
+import 'package:chat_app/constants.dart';
 import 'package:chat_app/cubits/chat_cubit/chat_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(InitialChatState());
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kMessagesCollections);
+
+  void sendMessage({required String message, required String email}) {
+    messages.add({
+      kMessage: message,
+      kCreatedAt: DateTime.now(),
+      'id': email,
+    });
+  }
+
+  void getMessages() {
+    messages.orderBy(kCreatedAt, descending: true).snapshots().listen((event) {
+      emit(SuccessChatState());
+    });
+  }
 }
--- lib/cubits/chat_cubit/chat_state.dart
+++ lib/cubits/chat_cubit/chat_state.dart
@@ -3,9 +3,3 @@ abstract class ChatState {}
 class InitialChatState extends ChatState {}
 
 class SuccessChatState extends ChatState {}
-
-class FailureChatState extends ChatState {
-  final String errorMessage;
-
-  FailureChatState(this.errorMessage);
-}
